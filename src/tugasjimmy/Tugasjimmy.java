e4b1374933edf5d54ccd1409d2611a6aa96580a2
dell <dell@Unayah>
2023-11-19 17:47:08 +0700

    tugasjimmy

--- src/tugasjimmy/Tugasjimmy.java
+++ src/tugasjimmy/Tugasjimmy.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package tugaspiii;
+package tugasjimmy;
 
 /**
  *
  * @author dell
  */
-public class TugaspIII {
+public class Tugasjimmy {
 
     /**
      * @param args the command line arguments
